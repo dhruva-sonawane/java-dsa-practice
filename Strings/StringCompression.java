b811afbcbb8ee4f346b9c4b0ed64699e9ffab978
Dhruva Sonawane <sonawanedhruva26@gmail.com>
2026-05-12 20:16:57 +0530

    Add StringCompression class for string compression

---
+++ Strings/StringCompression.java
@@ -0,0 +1,22 @@
+public class StringCompression {
+    public static String compress(String str) {
+        StringBuilder result = new StringBuilder();
+        int count = 1;
+
+        for (int i = 1; i <= str.length(); i++) {
+            if (i < str.length() && str.charAt(i) == str.charAt(i - 1)) {
+                count++;
+            } else {
+                result.append(str.charAt(i - 1)).append(count);
+                count = 1;
+            }
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        String input = "aaabbcc";
+        System.out.println("Compressed string: " + compress(input));
+    }
+}
