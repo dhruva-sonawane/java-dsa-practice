0e759579bad22c96e080c7d536388759736fef43
Dhruva Sonawane <sonawanedhruva26@gmail.com>
2026-05-12 20:04:16 +0530

    Add TwoSum class with main method for testing
    Implement TwoSum algorithm to find indices of two numbers that add up to a target.

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class TwoSum {
+    public static int[] twoSum(int[] arr, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int complement = target - arr[i];
+
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(arr[i], i);
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = twoSum(numbers, target);
+
+        if (result[0] != -1) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No valid pair found.");
+        }
+    }
+}
