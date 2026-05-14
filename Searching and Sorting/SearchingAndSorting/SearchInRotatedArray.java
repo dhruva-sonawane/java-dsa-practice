4740bf898edb68cb3e14bbab0c98296e4e949e5e
Dhruva Sonawane <sonawanedhruva26@gmail.com>
2026-05-14 16:56:55 +0530

    Add files via upload

---
+++ Searching and Sorting/SearchingAndSorting/SearchInRotatedArray.java
@@ -0,0 +1,17 @@
+public class SearchInRotatedArray {
+    public static int search(int[] arr, int target) {
+        int left = 0, right = arr.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) return mid;
+            if (arr[left] <= arr[mid]) {
+                if (target >= arr[left] && target < arr[mid]) right = mid - 1;
+                else left = mid + 1;
+            } else {
+                if (target > arr[mid] && target <= arr[right]) left = mid + 1;
+                else right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
